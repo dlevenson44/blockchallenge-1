ca5576c9d25aa4b3c42f3a4bdaa08c9dd9a8f837
Daniel Levenson <daniellevenson4@gmail.com>
2018-03-16 11:15:58 -0600

    added more content to btccontroller

--- src/components/BtcController.jsx
+++ src/components/BtcController.jsx
@@ -2,22 +2,22 @@ import React from 'react'
 
 const BtcController = (props) => {
     // convert strings to numbers for rounding
-    let test = parseFloat(props.btcPolo.high24hr)
+    let high24Hr = parseFloat(props.btcPolo.high24hr)
+    let low24Hr = parseFloat(props.btcPolo.low24hr).toFixed(2)
     
     return(
         <div>
             <h1>BTC -CapCoin</h1>
             <p>${props.btcCapCoin.usd} per BTC</p>
             <h5>BTC Trends:</h5>
-            <ul>
-                <li>{props.btcCapCoin.trends.oneHour}</li>
-                <li>{props.btcCapCoin.trends.oneDay}</li>
-                <li>{props.btcCapCoin.trends.oneWeek}</li>
-            </ul>
+                <p>{props.btcCapCoin.trends.oneHour}% change in last hour</p>
+                <p>{props.btcCapCoin.trends.oneDay}% change in last 24 hours</p>
+                <p>{props.btcCapCoin.trends.oneWeek}% change in last 7 days</p>
             <h1>BTC - Kraken</h1>
             <p>€{props.btcKraken.eur} per BTC</p>
             <h1>BTC - btc pOLO</h1>
-            <p>{test}</p>
+            <p>${high24Hr} is the 24 hour high</p>
+            <p>${low24Hr} is the 24 hour low</p>
         </div>
     )
 }
