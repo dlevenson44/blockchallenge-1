8ca751e037d85b61bc7538faaf0166f902e4c514
Daniel Levenson <daniellevenson4@gmail.com>
2018-03-16 11:40:18 -0600

    created and added skeleton content for ltc and eth controllers

---
+++ src/components/EthController.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const EthController = (props) => {
+    return(
+        <div>
+            <h1>eth controller</h1>
+        </div>
+    )
+}
+
+export default EthController
---
+++ src/components/LtcController.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LtcController = (props) => {
+    return(
+        <div>
+            <h1>ltc controller</h1>
+        </div>
+    )
+}
+
+export default LtcController
