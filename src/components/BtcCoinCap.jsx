b0179f46400b6651371d29dd3f3a104810b97ad7
Daniel Levenson <daniellevenson4@gmail.com>
2018-03-16 10:43:22 -0600

    redone to functional component

--- src/components/BtcCoinCap.jsx
+++ src/components/BtcCoinCap.jsx
@@ -1,69 +1,71 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class BtcCoinCap extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            btcCapCoin: {
-                usd: 0,
-                trends: {
-                    oneHour: 0,
-                    oneDay: 0,
-                    oneWeek: 0,
-                }
-            }
-        }
-        this.btcCapCoin = this.btcCapCoin.bind(this)
-    }
-
-    componentWillMount() {
-        this.btcCapCoin()
-    }
-
-    btcCapCoin() {
-        fetch('https://api.coinmarketcap.com/v1/ticker/bitcoin/?convert=USD', {
-            method: 'GET',
-        }).then(res => res.json())
-        .then(res => {
-            this.setState({                
-                btcCapCoin: {
-                    usd: res[0].price_usd,
-                    trends: {
-                        oneHour: res[0].percent_change_1h,
-                        oneDay: res[0].percent_change_24h,
-                        oneWeek: res[0].percent_change_7d
-                    }                    
-                }
-            })
-        })
-    }
-
-    render() {
-        return(
-            <div>
-                <h1>BitCoin</h1>
-                <p>${this.state.btcCapCoin.usd} per bitcoin</p>
-                <p>{this.state.btcCapCoin.trends.oneHour}% change in last hour</p>
-                <p>{this.state.btcCapCoin.trends.oneDay}% change in last day</p>
-                <p>{this.state.btcCapCoin.trends.oneWeek}% change in last week</p>
-            </div>
-        )
-    }
+const BtcCoinCap = (props) => {
+    return(
+        <div>
+            <h1>Bitcoin</h1>
+            <p>${props.btcCapCoin.usd} per BTC</p>
+            <h5>BTC Trends:</h5>
+            <ul>
+                <li>{props.btcCapCoin.trends.oneHour}</li>
+                <li>{props.btcCapCoin.trends.oneDay}</li>
+                <li>{props.btcCapCoin.trends.oneWeek}</li>
+            </ul>
+        </div>
+    )
 }
 
-// const BTCUSD = (props) => {
-//     return(
-//         <div>
-//             <h1>Bitcoin</h1>
-//             <p>${props.btc.usd} per BTC</p>
-//             <h5>BTC Trends:</h5>
-//             <ul>
-//                 <li>{props.btc.trends.oneHour}</li>
-//                 <li>{props.btc.trends.oneDay}</li>
-//                 <li>{props.btc.trends.oneWeek}</li>
-//             </ul>
-//         </div>
-//     )
-// }
-
 export default BtcCoinCap
+
+// import React, { Component } from 'react'
+
+// class BtcCoinCap extends Component {
+//     constructor(props) {
+//         super(props)
+//         this.state = {
+//             btcCapCoin: {
+//                 usd: 0,
+//                 trends: {
+//                     oneHour: 0,
+//                     oneDay: 0,
+//                     oneWeek: 0,
+//                 }
+//             }
+//         }
+//         this.btcCapCoin = this.btcCapCoin.bind(this)
+//     }
+
+//     componentWillMount() {
+//         this.btcCapCoin()
+//     }
+
+//     btcCapCoin() {
+//         fetch('https://api.coinmarketcap.com/v1/ticker/bitcoin/?convert=USD', {
+//             method: 'GET',
+//         }).then(res => res.json())
+//         .then(res => {
+//             this.setState({                
+//                 btcCapCoin: {
+//                     usd: res[0].price_usd,
+//                     trends: {
+//                         oneHour: res[0].percent_change_1h,
+//                         oneDay: res[0].percent_change_24h,
+//                         oneWeek: res[0].percent_change_7d
+//                     }                    
+//                 }
+//             })
+//         })
+//     }
+
+//     render() {
+//         return(
+//             <div>
+//                 <h1>BitCoin</h1>
+//                 <p>${this.state.btcCapCoin.usd} per bitcoin</p>
+//                 <p>{this.state.btcCapCoin.trends.oneHour}% change in last hour</p>
+//                 <p>{this.state.btcCapCoin.trends.oneDay}% change in last day</p>
+//                 <p>{this.state.btcCapCoin.trends.oneWeek}% change in last week</p>
+//             </div>
+//         )
+//     }
+// }
