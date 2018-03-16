8d0e623edf1e50e42631ed850d921eea0fd09ec0
Daniel Levenson <daniellevenson4@gmail.com>
2018-03-16 00:15:25 -0600

    changed btcusd to class component

--- src/components/BTCUSD.jsx
+++ src/components/BTCUSD.jsx
@@ -1,18 +1,72 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const BtcTracker = (props) => {
-    return(
-        <div>
-            <h1>Bitcoin</h1>
-            <p>${props.btc.value} per BTC</p>
-            <h5>BTC Trends:</h5>
-            <ul>
-                <li>{props.btc.trends.oneHour}</li>
-                <li>{props.btc.trends.oneDay}</li>
-                <li>{props.btc.trends.oneWeek}</li>
-            </ul>
-        </div>
-    )
+class BTCUSD extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            btcCapCoin: {
+                usd: 0,
+                trends: {
+                    oneHour: 0,
+                    oneDay: 0,
+                    oneWeek: 0,
+                }
+            }
+        }
+        this.btcCapCoin = this.btcCapCoin.bind(this)
+    }
+
+    componentWillMount() {
+        this.btcCapCoin()
+    }
+
+    btcCapCoin() {
+        fetch('https://api.coinmarketcap.com/v1/ticker/bitcoin/?convert=USD', {
+            method: 'GET',
+        }).then(res => res.json())
+        .then(res => {
+            console.log(res[0].price_usd)
+            this.setState({                
+                btcCapCoin: {
+                    usd: res[0].price_usd,
+                    trends: {
+                        oneHour: res[0].percent_change_1h,
+                        oneDay: res[0].percent_change_24h,
+                        oneWeek: res[0].percent_change_7d
+                    }                    
+                }
+            })
+        })
+        // window.location.reload()
+    }
+
+    render() {
+        console.log(this)
+        return(
+            <div>
+                <h1>BitCoin</h1>
+                <p>${this.state.btcCapCoin.usd} per bitcoin</p>
+                <p>{this.state.btcCapCoin.trends.oneHour}% change in last hour</p>
+                <p>{this.state.btcCapCoin.trends.oneDay}% change in last day</p>
+                <p>{this.state.btcCapCoin.trends.oneWeek}% change in last week</p>
+            </div>
+        )
+    }
 }
 
-export default BtcTracker
+// const BTCUSD = (props) => {
+//     return(
+//         <div>
+//             <h1>Bitcoin</h1>
+//             <p>${props.btc.usd} per BTC</p>
+//             <h5>BTC Trends:</h5>
+//             <ul>
+//                 <li>{props.btc.trends.oneHour}</li>
+//                 <li>{props.btc.trends.oneDay}</li>
+//                 <li>{props.btc.trends.oneWeek}</li>
+//             </ul>
+//         </div>
+//     )
+// }
+
+export default BTCUSD
