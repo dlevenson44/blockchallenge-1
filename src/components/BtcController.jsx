4a1d3b3f3d91a36de9eb40ffd85f1850dd4306fe
Daniel Levenson <daniellevenson4@gmail.com>
2018-03-16 10:53:47 -0600

    consolidated btc coincap and kraken components into controller

--- src/components/BtcController.jsx
+++ src/components/BtcController.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
 
-import BtcCoinCap from './BtcCoinCap'
-import BtcPolo from './BtcPolo'
-import BtcKraken from './BtcKraken'
-
-const BtcController = () => {
+const BtcController = (props) => {
     return(
         <div>
-            <BtcCoinCap />
-            <BtcPolo />
-            <BtcKraken />
+            <h1>BTC -CapCoin</h1>
+            <p>${props.btcCapCoin.usd} per BTC</p>
+            <h5>BTC Trends:</h5>
+            <ul>
+                <li>{props.btcCapCoin.trends.oneHour}</li>
+                <li>{props.btcCapCoin.trends.oneDay}</li>
+                <li>{props.btcCapCoin.trends.oneWeek}</li>
+            </ul>
+            <h1>BTC - Kraken</h1>
+            <p>€{props.btcKraken.eur} per BTC</p>
         </div>
     )
 }
 
 export default BtcController
+
+
