2776827841b353b1fc550dd24e0af083cb92c875
Daniel Levenson <daniellevenson4@gmail.com>
2018-03-16 00:16:10 -0600

    moved api fetch out of app.js

--- src/App.js
+++ src/App.js
@@ -3,78 +3,88 @@ import React, { Component } from 'react';
 import './App.css';
 
 // import components
-import BtcTracker from './components/BtcTracker'
+import BTCUSD from './components/BTCUSD'
 
 class App extends Component {
   constructor(props) {
     super(props)
-    this.state = {
-      btc: {
-        usd: 0,
-        trends: {
-          oneHour: 0,
-          oneDay: 0,
-          oneWeek: 0
-        }
-      },
-      dash: {
-        usd: 0,
-        trends: {
-          oneHour: 0,
-          oneDay: 0,
-          oneWeek: 0
-        }
-      },
-      eth: {
-        usd: 0,
-        trends: {
-          oneHour: 0,
-          oneDay: 0,
-          oneWeek: 0
-        }
-      },
-      ltc: {
-        usd: 0,
-        trends: {
-          oneHour: 0,
-          oneDay: 0,
-          oneWeek: 0,
-        }
-      }
+    this.state = {      
+      selectedCurrency: ''
     }
-    this.getBtcTicker = this.getBtcTicker.bind(this)
-  }
-
-  componentWillMount() {
-    this.getBtcTicker()
-  }
-
-  getBtcTicker() {
-    fetch('https://api.coinmarketcap.com/v1/ticker/bitcoin/?convert=USD', {
-      method: 'GET',
-    }).then(res => res.json())
-    .then(res => {
-      this.setState({
-        btc: {
-          usd: res[0].price_usd,
-          trends: {
-            oneHour: res[0].percent_change_1h,
-            oneDay: res[0].percent_change_24h,
-            oneWeek: res[0].percent_change_7d
-          }
-        }
-      })
-    })
+    // this.btcCapCoin = this.btcCapCoin.bind(this)
   }
 
   render() {
     return (
       <div className="App">
         <h1>hello world</h1>
-        <BtcTracker btc={this.state.btc} />
+        <BTCUSD />
       </div>
     );
   }
 }
 
 export default App;
+
+
+
+// btc: {
+//   usd: 0,
+//   eur: 0,
+//   eurInfo: {
+//     ask: 0,
+//     bid: 0,
+//     high: 0,
+//     low: 0
+//   },
+//   trends: {
+//     oneHour: 0,
+//     oneDay: 0,
+//     oneWeek: 0
+//   }
+// },
+// dash: {
+//   usd: 0,
+//   eur: 0,
+//   eurInfo: {
+//     ask: 0,
+//     bid: 0,
+//     high: 0,
+//     low: 0
+//   },
+//   trends: {
+//     oneHour: 0,
+//     oneDay: 0,
+//     oneWeek: 0
+//   }
+// },
+// eth: {
+//   usd: 0,
+//   eur: 0,
+//   eurInfo: {
+//     ask: 0,
+//     bid: 0,
+//     high: 0,
+//     low: 0
+//   },
+//   trends: {
+//     oneHour: 0,
+//     oneDay: 0,
+//     oneWeek: 0
+//   }
+// },
+// ltc: {
+//   usd: 0,
+//   eur: 0,
+//   eurInfo: {
+//     ask: 0,
+//     bid: 0,
+//     high: 0,
+//     low: 0
+//   },
+//   trends: {
+//     oneHour: 0,
+//     oneDay: 0,
+//     oneWeek: 0
+//   }
+// }
