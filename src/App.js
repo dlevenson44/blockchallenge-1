59b4a494c0a0f7a3bb312b64f3fa68081d1b08fd
Daniel Levenson <daniellevenson4@gmail.com>
2018-03-15 19:19:40 -0600

    fetching bitcoin data from coincap

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      btcTicker: '',
+      ethTicker: '',
+      dashTicker: '',
+      ltcTicker: ''
+    }
+    this.getBtcTicker = this.getBtcTicker.bind(this)
+  }
+
+  componentDidMount() {
+    this.getBtcTicker()
+  }
+
+  getBtcTicker() {
+    fetch('https://api.coinmarketcap.com/v1/ticker/bitcoin/?convert=USD', {
+      method: 'GET',
+    }).then(res => res.json())
+    .then(res => {
+      // console.log(res[0])
+      this.setState({
+        btcTicker: res[0]
+      })
+    })
+  }
+
   render() {
+    console.log(this.state.btcTicker)
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>hello world</h1>
+        <h3>{this.state.btcTicker.name}</h3>
       </div>
     );
   }
