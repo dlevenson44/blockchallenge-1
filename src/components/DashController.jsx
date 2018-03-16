ae4daa7b4785bccafb496ee6340a2510ed44a8e2
Daniel Levenson <daniellevenson4@gmail.com>
2018-03-16 11:22:41 -0600

    pulling dash capcoin into component

--- src/components/DashController.jsx
+++ src/components/DashController.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
-const DashController = () => {
+const DashController = (props) => {
+    let usd = parseFloat(props.dashCapCoin.usd).toFixed(2)
+
     return(
         <div>
-            <h1>sup duggy</h1>
+            <h1>DASH-- CapCoin</h1>
+            <p>${usd} per DASH</p>
         </div>
     )
 }
