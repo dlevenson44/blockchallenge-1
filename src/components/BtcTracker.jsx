35bea866b0d21dd377d40990373f2459231c1790
Daniel Levenson <daniellevenson4@gmail.com>
2018-03-15 19:20:55 -0600

    created btctracker

---
+++ src/components/BtcTracker.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BtcTracker = (props) => {
+    return(
+        <div>
+            <h1>hello btctracker</h1>
+        </div>
+    )
+}
+
+export default BtcTracker
