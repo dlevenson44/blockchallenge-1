71a226f395004a0d39165604551fa1048adbc999
Daniel Levenson <daniellevenson4@gmail.com>
2018-03-15 20:19:26 -0600

    updated component content

--- src/components/BtcTracker.jsx
+++ src/components/BtcTracker.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 const BtcTracker = (props) => {
     return(
         <div>
-            <h1>{props.btcTracker.name}</h1>
+            <h1>Bitcoin</h1>
+            <p>${props.btc.usd} per BTC</p>
+            <h5>BTC Trends:</h5>
+            <ul>
+                <li>{props.btc.trends.oneHour}</li>
+                <li>{props.btc.trends.oneDay}</li>
+                <li>{props.btc.trends.oneWeek}</li>
+            </ul>
         </div>
     )
 }
