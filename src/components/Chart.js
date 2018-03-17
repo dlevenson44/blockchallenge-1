b7bd4a3a28221aa8faa7f1252f048ae33a15732b
Daniel Levenson <daniellevenson4@gmail.com>
2018-03-16 22:17:00 -0600

    created and added content to chart component

---
+++ src/components/Chart.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Bar, Line, Pie } from 'react-chartjs-2'
+
+class Chart extends Component {
+    render() {
+        return(
+            <div>
+                <h1>charts</h1>
+            </div>
+        )
+    }
+}
+
+export default Chart
