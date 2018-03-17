5ce7468bfff1a52c5d953a18c1d5425e3cb4d09c
Daniel Levenson <daniellevenson4@gmail.com>
2018-03-17 17:59:48 -0600

    removed proptype import

--- src/components/BtcChart.js
+++ src/components/BtcChart.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import { Doughnut } from 'react-chartjs-2'
 
 class BtcChart extends Component {
--- src/components/DollarChart.js
+++ src/components/DollarChart.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import { Bar } from 'react-chartjs-2'
 
 class DollarChart extends Component {
