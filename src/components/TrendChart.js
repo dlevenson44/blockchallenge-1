a7caf1b39dbefaffdd5763ecdad057cb70cb7f3b
Daniel Levenson <daniellevenson4@gmail.com>
2018-03-17 15:48:47 -0600

    created and set up trendchart

---
+++ src/components/TrendChart.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Bar } from 'react-chartjs-2'
+
+class TrendChart extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return(
+            <div>
+                <h1>hello world</h1>
+            </div>
+        )
+    }
+}
+
+export default TrendChart
