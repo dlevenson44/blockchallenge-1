86768af76d171610d9c990cdc72ad2cbb793badb
Daniel Levenson <daniellevenson4@gmail.com>
2018-03-17 21:27:41 -0600

    set up btccontroller

--- src/components/BtcController.jsx
+++ src/components/BtcController.jsx
@@ -23,27 +23,24 @@ const BtcController = (props) => {
     let ltcPerBtc = (btc / ltc).toFixed(2)
 
     return(
-        <div>
-            <h1>BTC -CapCoin</h1>
-            <p>${usd} per BTC</p>
-            <h5>BTC Trends:</h5>
-                <p>{props.btcCapCoin.oneHour}% change in last hour</p>
-                <p>{props.btcCapCoin.oneDay}% change in last 24 hours</p>
-                <p>{props.btcCapCoin.oneWeek}% change in last 7 days</p>
-            <h1>BTC - Kraken</h1>
-            <p>€{euro} per BTC</p>
-            <p>€{highBid} is the high bid</p>
-            <p>€{lowAsk} is the low ask</p>            
-            <p>€{euHigh24Hr} is the 24 hour high</p>
-            <p>€{euLow24Hr} is the 24 hour low</p>
-            <p>{props.btcKraken.trends.trades} trades in the last 24 hours</p>
-            <h1>BTC - btc pOLO</h1>
-            <p>${usHigh24Hr} is the 24 hour high</p>
-            <p>${usLow24Hr} is the 24 hour low</p>
-            <h3>ALT per BTC values</h3>
-            <p>{dashPerBtc} DASH per BTC</p>
-            <p>{ethPerBtc} ETH per BTC</p>
-            <p>{ltcPerBtc} LTC per BTC</p>
+        <div className="crypto-container">
+            <h3>Crypt Exchange Rates</h3>
+                <p>{dashPerBtc} DASH per BTC</p>
+                <p>{ethPerBtc} ETH per BTC</p>
+                <p>{ltcPerBtc} LTC per BTC</p>
+                <h5>Trends:</h5>
+                    <p>{props.btcKraken.trends.trades} trades in the last 24 hours</p>
+                    <p>{props.btcCapCoin.oneHour}% change in last hour</p>
+                    <p>{props.btcCapCoin.oneDay}% change in last 24 hours</p>
+                    <p>{props.btcCapCoin.oneWeek}% change in last 7 days</p>                
+            <h3>BTC US Market Info</h3>
+                <p>${usd} per BTC</p>
+                <p>${usHigh24Hr} is the 24 hour high</p>
+                <p>${usLow24Hr} is the 24 hour low</p>                            
+            <h3>BTC EU Market Info</h3>
+                <p>€{euro} per BTC</p>
+                <p>€{euHigh24Hr} is the 24 hour high</p>
+                <p>€{euLow24Hr} is the 24 hour low</p>            
         </div>
     )
 }
